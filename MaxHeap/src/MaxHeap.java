e17cce9227ce01f2713e9419dcbccdd8ebb09463
Gopal Menon <gopal.mnn@gmail.com>
2015-12-28 15:24:45 -0700

    Added maxheapify and buyild max heap code

--- MaxHeap/src/MaxHeap.java
+++ MaxHeap/src/MaxHeap.java
@@ -17,6 +17,8 @@ public class MaxHeap {
 			
 		}
 		
+		//Make this a max heap
+		buildMaxHeap();
 	}
 	
 	private int getParent(int index) {
@@ -31,7 +33,45 @@ public class MaxHeap {
 		return index * 2 + 1;
 	}
 	
-	private void MaxHeapify(int rootIndex) {
+	//Build a max heap at the given index. Assumption is that both left and right sub-trees
+	//are already max heaps
+	private void maxHeapify(int rootIndex) {
+		
+		int largestIndex = 0;
+		int leftChild = getLeftChild(rootIndex);
+		int rightChild = getRightChild(rootIndex);
+		
+		//Find the largest index out of the left and right children
+		if (leftChild <= this.heapSize - 1 && this.internalRepresentation[leftChild] > this.internalRepresentation[rootIndex]) {
+			largestIndex = leftChild;
+		} else {
+			largestIndex = rootIndex;
+		}
+		
+		if (rightChild <= this.heapSize - 1 && this.internalRepresentation[rightChild] > this.internalRepresentation[largestIndex]) {
+			largestIndex = rightChild;
+		} else {
+			largestIndex = rootIndex;
+		}
+		
+		//Swap with root if root is not largest. Then run MaxHeapify on the subtree rooted at the largest index
+		if (largestIndex != rootIndex) {
+
+			int tempValue = this.internalRepresentation[rootIndex];
+			this.internalRepresentation[rootIndex] = this.internalRepresentation[largestIndex];
+			this.internalRepresentation[largestIndex] = tempValue;
+			
+			maxHeapify(largestIndex);
+		}
+		
+	}
+	
+	//Build a max heap from the internal representation array
+	private void buildMaxHeap() {
+		
+		for (int index = this.heapSize / 2; index >= 0; --index) {
+			maxHeapify(index);
+		}
 		
 	}
 
