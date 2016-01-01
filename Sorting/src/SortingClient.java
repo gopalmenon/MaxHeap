ab0c5b9733e136674f96274f6c32f782fa550e61
Gopal Menon <gopal.mnn@gmail.com>
2016-01-01 12:23:58 -0700

    Renamed MaxHeapClient to SortingClient

---
+++ Sorting/src/SortingClient.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+public class MaxHeapClient {
+
+	public static final int HEAP_SIZE = 100;
+	public static void main(String[] args) {
+		
+		MaxHeapClient maxHeapClient = new MaxHeapClient();
+		maxHeapClient.testMaxHeap();
+
+	}
+	
+	private void testMaxHeap() {
+		
+		int[] unsortedArray = new int[HEAP_SIZE];
+		Random randomNumberGenerator = new Random();
+		for (int index = 0; index < HEAP_SIZE; ++index) {
+			unsortedArray[index] = randomNumberGenerator.nextInt();
+		}
+		
+		MaxHeap maxHeap = new MaxHeap(unsortedArray);
+		System.out.println(maxHeap.toString());
+		
+		maxHeap.heapSort();
+		System.out.println(maxHeap.toString());
+		
+	}
+
+}
